20d15d56915a64672a89f0c1d62748f82fe593e0
Travis Berry <travisberry@travisberry.com>
2010-01-11 20:22:42 -0700

    added blog tag line to footer

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 </div><div id="footerwide">
 <div id="footer">
-	&copy;2010 <?php bloginfo('name'); ?> | Created By: <a href="http://www.travisberry.com/">Travis Berry</a>
+	&copy;2010 <?php bloginfo('name'); ?> <?php get_bloginfo ( 'description' );  ?> | Created By: <a href="http://www.travisberry.com/">Travis Berry</a>
 		<?php wp_footer(); ?></div></div>
 
 </body>
